834bbe412461e0ce0e27eff6aa3caaef183a1c95
kavikeerthana05 <kaviranjananarv@gmail.com>
2026-01-26 14:16:48 +0530

    Initial project code upload

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 """
 Main execution script.
 """
-
 import pandas as pd
 from torch.utils.data import DataLoader
 from datasets.dataset import TimeSeriesDataset
 from models.attention_seq2seq import AttentionSeq2Seq
+from training.training import train_model
+from evaluation.metrics import mae, rmse
 
 df = pd.read_csv("data/time_series.csv")
 
